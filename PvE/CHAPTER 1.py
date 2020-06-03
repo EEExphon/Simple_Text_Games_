59e03bf054698c02dc988697c5d9f3e55d41006a
JJiong <66343319+EEExphon@users.noreply.github.com>
2020-06-03 16:58:14 +0800

    text games
    python

---
+++ PvE/CHAPTER 1.py
@@ -0,0 +1,143 @@
+ #   <<VAN DEEP>> PRODUCED BY RICHARD余世炅
+print("===================================================================================================")
+print("===================================================================================================")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++\        /     /\         /\        /++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++++++++++\      /     /  \       /  \      /+++++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++++\    /     /----\     /    \    /++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++++++++++++\  /     /      \   /      \  /+++++++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++++++\/     /        \ /        \/++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++|----\    |----  |----   |---\++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++|     |   |      |       |   |++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++|     |   |----  |----   |---/++++++++++++++++++++++++++++++++++++++++++++++++++") 
+print("+++++++++++++++++++|     |   |      |       |    ++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++|----/    |----  |----   |    ++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("++++++++++++++++++++++++++++++++++++----BY:RICHARD-----++++++++++++++++++++++++++++++++++++++++++++")
+print("_______________________________________WORKING TEAM : BRICONIS_____________________________________")
+print("_________________________________________________________________________________1.1.2_____________")
+print("===================================================================================================")
+print("===================================================================================================")
+print("                                             ")
+print("    ")
+input("                             PRESS ENTER TO BIGIN THIS GAME.")
+print("                       REMEMBER TO PRESS ENTER TO CONTINUE THE STORY")
+print("                    AND DEFINRTELY INPUT THE WORDS THAT AFFECT THIS GAME")
+PASSWORD=input("               NOW PLEASE GIVE ME YOUR PASSWORD:")
+print("                         SO YOUR PASSWORD IS",PASSWORD)
+print("     ")
+NUM0=input("                              WELCOME TO VAN")
+if NUM0=="DEEP DARK FANTASY":
+    print("                 乖乖站好！！！！BBBOOOOOOOOOOOOOOOOOOOOOKI!!!!!!!")
+else:
+    print("      ")
+USERNAME=input(             "A voice that seems is really far from you:YO,KID,WHAT'S YOUR NAME?")
+print("                      Still that voice:OK",USERNAME)
+AGE=input("                  HOW OLD ARE YOU?")
+print("                      Still that voice:really?",AGE,"?FINE.")
+input("             The voice that you've heard two times:MY NAME IS THE GREATEST RICHARD.")
+NUM9=input("             THE GREATEST RICHARD:THIS GAME IS CALLED VAN.")
+if NUM9=="DEEP DARK FANTASY":
+    print("         THE GREATEST RICHARD:HAHAHAHAHAHAHAHAHAHAHA....乖乖站好啊~~~~")
+input("             THE GREATEST RICHARD:PRETTY GOOD,HAH?")
+input("             THE GREATEST RICHARD:NOW THE GAME BIGINS.")
+TONY=100
+CHRIS=100
+print("___________________________________________________________________________________________________")
+print("                    C    H    A    P    T    E    R        O   N   E")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+input("                                       PRESS ENTER TO START     ")
+print("             TGR:HEY KID,ARE YOU READY TO HAVE SOME FUN?")
+NUM1=input("you can say yes or no  ")
+if NUM1 =="yes":
+    print("         TGR:OK,FINE.")
+else :
+    print("         TGR:WELL,THAT DOESN'T DEPEND ON YOU.")
+input("             TGR:THIS IS YOUR MATES---THE MOST RELIABLE CHRIS.AND THE TALLEST TONY.")
+print("             TGR:IF YOU MEET ")
+print("                 A NEW MONSTER.CHRIS WILL TELL YOU THE SPECIAL ABILITY THAT THE MONSTER HAVE.")
+print("                 AND TONY IS YOUR MATE WHO CAN HEIP YOU WHEN YOU DON'T KNOW HOW TO KILL A MONSTER.")
+print("                 YOU CAN LET HIM HELP YOU BY GIVING A PASSWORD THAT YUSHIJIONG GIVES YOU.")
+print("                 TO GET THE PASSWORD,YOU HAVE TO ANSWER THE QUESTION YUSHIJIONG ASKS YOU.")
+print("                 OR PAY FOR IT.       2      RMB/TIME")
+NUM2=input("you can say ok or what the hell or that's fine")
+if NUM2=="ok":
+    print("                TGR:GOOD KID.")
+if NUM2=="what the hell":
+    print("                TGR:YEAH?HELL HAVE YOU EVER BEEN TO THE HELL?")
+if NUM2=="that's fine":
+    print("                TGR:GOOD KID.I LIKE YOUR CONFIDENCE")
+else:
+    print("          ")
+input("  余世炅说的话：游戏问题声明一下啊。由于技术有限。")
+input("  如果你发现自己输入的回答没有电脑的回复。可能是由于大小写出了问题。或者是空格。我们在设计的时候是只")
+input("  有小写。没有大写的情况。也没有句点希望大家谅解。还有。出了错的话请重新进入本file的游戏.")
+input("  好了请大家继续游戏吧。")
+input("                    TGR:NOW LET'S DO SOME EXERCISE.")
+print("                        LOOK AT THIS MONSTER!")
+print("                        I LETED IT TO DRINK SOME VAN ENERGY.DO YOU LIKE IT?")
+input("                    TMRC:THIS MONSTER HAVE 'REVENGE' ABILITY!IT WILL BLOW ITSELF AFTER IT DIED")
+print("                    TGR:",USERNAME,",DO YOU LIKE IT?")
+NUM3=input("you can say yes or no.")
+if NUM3=="yes":
+    print("              TGR:OKAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY!!!")
+if NUM3=="no":
+    print("              TGR:WELL......GIVE IT A TRY!!!")
+else:
+    print("     ")
+input("                                  NOW. FACE THAT MONSTER")
+print("                      INFORMATION ABOUT THE MONSTER:HP50,SPE:REVENGE")
+print("  余世炅又来了啊。我现在给大家讲解一下我们的战斗模式。战斗模式叫做VAN。其实就是摔跤。但你们不会有操作。")
+print("  我们会提供A~E一共五个连招方案。每次打怪都要玩家精确计算伤害。并不是伤害最大就最好。每个怪物有不同的的能力。")
+print("  主角拥有四个技能，也有平a,SP=10。")
+print("  1-----强化：一回合内强化技能或者平A。伤害*1.5       可以与平a一起作为一回合")
+print("  2-----伤害：立刻伤害20                        不可以与平a一起作为一回合")
+print("  3-----增强：此技能以后角色使用一个技能，下一个回合开始，伤害加百分之十，并且角色获得剩余HP的百分之五十的只存在一局的护盾。")
+print("                                                 可以与平a一起作为一回合")
+print("  4-----终结：立刻造成上回合伤害的两倍。        不可以与平a一起作为一回合")
+print("  你也可以通过回答炅炅的问题来获得答案，但这会影响一点游戏体验哦，不提倡。而且你永远是先手。")
+print("  我只有三个问题，密码只能在普通的怪那里用，boss没有密码。")
+input("  继续吧孩纸！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！")
+print("  PLAN---A:  1a-3a-2-1a-4")
+print("  PLAN---B:  3a-1a-4-2-1a")
+print("  PLAN---C:  a-a-a-a-a")
+print("  PLAN---D:  1a-1a-1a-1a-1a")
+print("  PLAN---E:  3a-2-1a-1a-4")
+NUM4=input("please choose your plan.fromA~E")
+if NUM3=="A":
+    print("                        OK!!!!!!!!!!IT'S DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+if NUM3=="B":
+    print("                        OK!!!!!!!!!!IT'S DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
+if NUM3=="C":
+    print("                        OK!!!!!!!!!!IT'S DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+if NUM3=="D":
+    print("                        OK!!!!!!!!!!IT'S DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+if NUM3=="E":
+    print("                        OK!!!!!!!!!!IT'S DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+else:
+    print("  ")
+input("           THE BIG MONSTER:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAORRRRRRRR")
+input("           CHRIS:IT IS GOING TO BLOW ITSELF!!!!!!!!!!RUN!!!!!!!!!!!")
+input("           TONY;OKOK,I'M GOING TO USE MY VANPOWER!!!SEE?I'M TAAAAAAAAAAAAAA~~~~LLLLLLERRRRRRRR")
+NUM5=input("you can      go on tony's hand            or                    go to his head")
+if NUM5=="go on tony's hand":
+    print("             TONY:OKOK,LET'S GOOOOOOOOOOOOOOOOOOOOOOOOOOO!")
+if NUM5=="go to his head":
+    print("             TONY:OKOK,WELL.GET OFF MY HEAD!!SIT ON MY HAND")
+else:
+    print("    ")
+print("             TONY:LET'S RUN!!!!!!!!!!!!")
+print("_____________________________________________________________________________________________________________")
+print("                      E   N   D     O   F     C   H   A   P   T   E   R     O   N   E")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("第一章结束了，这一章大部分都是教程，下一章就开始冒险啦！！！！！！！！！！！！！！！！！！！")
+print("加油鸭孩纸！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！")
+print("等余世炅做完第二章了你就继续玩啦！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！")
+
+
+
+
+
+
---
+++ PvP/Fight till the end/1.1.4 KRBT.py
@@ -0,0 +1,377 @@
+print("                     Welcome to VAN (PvP) by Richard.")
+print("                                                        1.1.4")
+
+input("     WE HAVE THREE HEROS:(press enter to continue)")
+print("     YOU CAN USE 3 AFTER THE 4TH ROUND AND 4ROUNDS AFTER.")
+print("     BRUCE : |HP=150|a_10|1_强化a或2|2_+20HP   |3_   +50HP  |")
+print("               THE HIGHEST HP IS 150")
+print("   RICHARD : |HP=150|a_15|1_+10HP   |2_  30    |3_  ANI'HP/2|")
+print("             生命值小于等于三十，a=20,2=40")
+print("      KATE : |HP=250|a_10|1_+30HP   |2_100HP   |3_伤害(250-HP)/2|")
+print("              THE HIGHEST HP IS 250")
+print("  TONY : |HP=150|a=10|1_三回合持续伤害，每回合10.|2_+20HP|3_伤害自己10HP，抉择：敌方HP-45 or 三回合持续攻击15/round|")
+print("         |4.消灭TONY并以LUIS大王代替之。LUIS：HP=100 a=20 1_ 30  2_+20HP   3_ 持续三回合攻击15/round|")
+
+PLAYER1=input("Now , player1 choose the hero:")
+
+while PLAYER1 != "BRUCE" and PLAYER1 != "RICHARD" and PLAYER1 != "KATE" and PLAYER1 != "TONY":
+    PLAYER1=input("Now , player1 choose the hero:")
+    
+if PLAYER1=="BRUCE":
+    HPI=150
+    NI=150
+if PLAYER1=="RICHARD":
+    HPI=150
+    NI=150
+if PLAYER1=="KATE":
+    HPI=250
+    NI=250
+if PLAYER1=="TONY":
+    HPI=150
+    NI=150
+
+    
+PLAYER2=input("Now , player2 choose the hero:")
+
+while PLAYER2 != "BRUCE" and PLAYER2 != "RICHARD" and PLAYER2 != "KATE" and PLAYER2 != "TONY":
+    PLAYER2=input("Now , player2 choose the hero:")
+    
+if PLAYER2=="BRUCE":
+    HPII=150
+    NII=150
+if PLAYER2=="RICHARD":
+    HPII=150
+    NII=150
+if PLAYER2=="KATE":
+    HPII=250
+    NII=250
+if PLAYER2=="TONY":
+    HPII=150
+    NII=150
+
+input("(press enter to continue)")
+
+print("               AND NOW THE GAME BEGINS")
+print("               IF YOU DIED IN 1-19 ROUND ")
+print("               YOU ARE NOT THE LOSER")
+print("               YOU CAN STILL ADD HP!!!!!")
+print("               NEVER GIVE UP!")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+for ROUND in range (1,21):
+    
+    print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("ROUND",ROUND)
+    
+    PL1=input("            NOW , PLAYER 1 CHOOSE WHAT TO DO.")
+    if PLAYER1=="BRUCE":
+        if PL1=="a":
+            HPII=HPII-10
+        if PL1=="1":
+            BNM1=input("a or 2")
+            if BNM1=="a":
+                if HPI<=140:
+                    HPI=HPI+10
+                else:
+                    HPI=150
+                HPII=HPII-10
+            if BNM1=="2":
+                if HPI<=130:
+                    HPI=HPI+20
+                else:
+                    HPI=150
+                HPII=HPII-5
+        if PL1=="2":
+            if HPI<=130:
+                HPI=HPI+20
+            else:
+                HPI=150
+        if PL1=="3":
+            if HPI<=100:
+                HPI=HPI+50
+            else:
+                HPI=150
+    if PLAYER1=="RICHARD":
+        if PL1=="a":
+            if HPI>30:
+                HPII=HPII-15
+            else:
+                HPII=HPII-20
+        if PL1=="1":
+            if HPI<=140:
+                HPI=HPI+10
+            else:
+                HPI=150
+        if PL1=="2":
+            if HPI>30:
+                HPII=HPII-30
+            else:
+                HPII=HPII-40
+        if PL1=="3":
+            HPII=HPII/2
+    if PLAYER1=="KATE":
+        if PL1=="a":
+            HPII=HPII-10
+        if PL1=="1":
+            if HPI<=220:
+                HPI=HPI+30
+            else:
+                HPI=250
+        if PL1=="2":
+            HPI=100
+        if PL1=="3":
+            HPII=HPII-(250-HPI)/2
+    if PLAYER1=="TONY":
+        TO1=0
+        TOTO1=0
+        MNMNMN1=1
+        if PL1=="a":
+            HPII=HPII-10
+        if PL1=="1":
+            TO1=4
+        if PL1=="2":
+            if HPI>=130:
+                HPI=150
+            if HPI<130:
+                HPI=HPI+20
+        if PL1=="3":
+            HPI=HPI-10
+            CHOOSE=input("choose 1 or 2:1-敌方HP-30 or 2-三回合持续攻击10/round")
+            if CHOOSE=="1":
+                HPII=HPII-45
+            if CHOOSE=="2":
+                TOTO1=4
+        if PL1=="4":
+            PLAYER1="LUIS"
+            print("You are Luis The King now!")
+            print(" ")
+            print("LUIS：HP=100 a=20 1_ 30  2_+20HP   3_ 持续三回合攻击15/round")
+            print(" ")
+            print("WRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY!!!")
+            print(" ")
+            HPI=100
+            NI=100
+        TO1=TO1-1
+        if TO1<=0:
+            TO1=0
+        if TO1==1 or TO1==2 or TO1==3:
+            HPII=HPII-10
+        TOTO1=TOTO1-1
+        if TOTO1<=0:
+            TOTO1=0
+        if TOTO1==1 or TOTO1==2 or TOTO1==3:
+            HPII=HPII-15
+            
+    if PLAYER1=="LUIS":
+        LUISGOOD1=0
+        if PL1=="a":
+            HPII=HPII-20
+        if PL1=="1":
+            HPII=HPII-30
+        if PL1=="2":
+            if HPI>=80:
+                HPI=100
+            if HPI<80:
+                HPI=HPI+20
+        if PL1=="3":
+            LUISGOOD1=4
+        
+        LUISGOOD1=LUISGOOD1-1
+        if LUISGOOD1<=0:
+            LUISGOOD1=0
+        if LUISGOOD1==1 or LUISGOOD1==2 or LUISGOOD1==3:
+            HPII=HPII-15
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    PL2=input("            NOW , PLAYER 2 CHOOSE WHAT TO DO.")
+    if PLAYER2=="BRUCE":
+        if PL2=="a":
+            HPI=HPI-10
+        if PL2=="1":
+            BNM2=input("a or 2")
+            if BNM2=="a":
+                if HPII<=140:
+                    HPII=HPII+10
+                else:
+                    HPII=150
+                HPI=HPI-10
+            if BNM2=="2":
+                if HPII<=130:
+                    HPII=HPII+20
+                else:
+                    HPII=150
+                HPI=HPI-5
+        if PL2=="2":
+            if HPII<=130:
+                HPII=HPII+20
+            else:
+                HPII=150
+        if PL2=="3":
+            if HPII<=100:
+                HPII=HPII+50
+            else:
+                HPII=150
+    if PLAYER2=="RICHARD":
+        if PL2=="a":
+            if HPII>30:
+                HPI=HPI-15
+            else:
+                HPI=HPI-20
+        if PL2=="1":
+            if HPII<=140:
+                HPII=HPII+10
+            else:
+                HPII=150
+        if PL2=="2":
+            if HPII>30:
+                HPI=HPI-30
+            else:
+                HPI=HPI-40
+        if PL2=="3":
+            HPI=HPI/2
+    if PLAYER2=="KATE":
+        if PL2=="a":
+            HPI=HPI-10
+        if PL2=="1":
+            if HPII<=220:
+                HPII=HPII+30
+            else:
+                HPII=250
+        if PL2=="2":
+            HPII=100
+        if PL2=="3":
+            HPI=HPI-(250-HPII)/2
+ 
+    if PLAYER2=="TONY":
+        TO2=0
+        TOTO2=0
+        MNMNMN2=1
+        if PL2=="a":
+            HPI=HPI-10
+        if PL2=="1":
+            TO2=4
+        if PL2=="2":
+            if HPII>=130:
+                HPII=150
+            if HPII<130:
+                HPII=HPII+20
+        if PL2=="3":
+            HPII=HPII-10
+            CHOOSE=input("choose 1 or 2:1-敌方HP-30 or 2-三回合持续攻击10/round")
+            if CHOOSE=="1":
+                HPI=HPI-45
+            if CHOOSE=="2":
+                TOTO2=4
+        if PL2=="4":
+            PLAYER2="LUIS"
+            print("You are Luis The King now!")
+            print(" ")
+            print("LUIS：HP=100 a=20 1_ 30  2_+20HP   3_ 持续三回合攻击15/round")
+            print(" ")
+            print("WRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY!!!")
+            print(" ")
+            HPII=100
+            NII=100
+        TO2=TO2-1
+        if TO2<=0:
+            TO2=0
+        if TO2==1 or TO2==2 or TO2==3:
+            HPI=HPI-10
+        TOTO2=TOTO2-1
+        if TOTO2<=0:
+            TOTO2=0
+        if TOTO2==1 or TOTO2==2 or TOTO2==3:
+            HPI=HPI-15
+            
+    if PLAYER2=="LUIS":
+        LUISGOOD2=0
+        if PL2=="a":
+            HPI=HPI-20
+        if PL2=="1":
+            HPI=HPI-30
+        if PL2=="2":
+            if HPII>=80:
+                HPII=100
+            if HPII<80:
+                HPII=HPII+20
+        if PL2=="3":
+            LUISGOOD2=4
+        
+        LUISGOOD2=LUISGOOD2-1
+        if LUISGOOD2<=0:
+            LUISGOOD2=0
+        if LUISGOOD2==1 or LUISGOOD2==2 or LUISGOOD2==3:
+            HPI=HPI-15
+ 
+    print("PLAYER ONE HP:",HPI,"/",NI)
+    print("PLAYER TWO HP:",HPII,"/",NII)
+    print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+print("HPI=",HPI,"_______HPII=",HPII)
+if HPI>HPII:
+    print("the winner is p1")
+if HPI<HPII:
+    print("the winner is p2")
+if HPII==HPI:
+    print("same.")
+ENDINGCHOICE=input("You can end this program by pressing enter . Or you can enter 'ending' to see the ending .")
+if ENDINGCHOICE=="ending" or ENDINGCHOICE=="ENDING":
+    print("THIS GAME IS PRESENTED BY =BRICONIS= .")
+    input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("WE WILL KEEP UPDATING THIS GAME UNTIL IT IS COMPLETELY COMPLETED .")
+    input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("FOR TEAM BRICONIS , WE HAVE BRUCE , RICHARD , TONY AND LUIS .")
+    input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("AND WE ARE ALL EXPECTING YOU TO HAVE A GOOD LIFE .")
+    input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("                                                                ___________                        ")
+    print("                                                                || BRUCE ||                        ")
+    print("                                                                ||RICHARD||                        ")
+    print("                                                                || TONY  ||                        ")
+    print("                                                                || LUIS  ||                        ")
+    print("                                                                ||<<<|>>>||                        ")
+    print("                                                                ||THANKS ||                        ")
+    print("                                                                ||  FOR  ||                        ")
+    print("                                                                ||PLAYING||                        ")
+    print("                    ++++++++++++++++++++++++++++++++++++++++++++~~~~~~~~~~+++++++                  ")
+    print("              =========================================================================            ")
+    print("        =====================================================================================      ")
+    print("  =================================================================================================")
+    print("  |||                                                                                           |||")
+    print("  |||     ____________________________________________________________________________          |||")
+    print("  |||     {YOU DON'T HAVE TO LOOK AT THIS ENDING EVERY TIME AFTER YOU PLAY THIS GAME.}          |||")
+    print("  |||     {THIS ENDING IS FOR THOSE PEOPLE WHO IS THE FIRST TIME PLAYING THIS GAME  .}          |||")
+    print("  |||     {WE MADE THIS GAME FOR YOU GUYS TO RELAX AND TO HAVE A LITTLE COMPETITION .}          |||")
+    print("  |||     {              AND WE WILL BE VERY GLAD IF YOU GUYS LIKE IT               .}          |||")
+    print("  |||     {I HAVE A LOT OF THINGS TO SAY , BUT I'M AFRAID OF THIS HOUSE WOULD BE TOO }          |||")
+    print("  |||     {                                    HIGH                                 .}          |||")
+    print("  |||     {WELL , I SUDDENLY REALIZED THAT IT IS NON OF MY BUSINESS HOW HIGH THIS    }          |||")
+    print("  |||     {                              HOUSE WILL BE                              .}          |||")
+    print("  |||     {HOWEVER , DO YOU THINK THIS IS A GOOD EXPERIENCE of.......................}          |||")
+    print("  |||     {..........................................................TALKING WITH ME?}          |||")
+    print("  |||     {WISH YOU HAVE A GOOD TIME DURING ENJOYING YOUR LIFE                      !}          |||")
+    print("  |||     {IF YOU THINK YOUR LIFE IS UNLUCKY.........................................}          |||")
+    print("  |||     {                         WISH YOU WILL BE  LUCKY TOMORROW                .}          |||")
+    print("  |||     {                                                 SEE YOU NEXT TIME!!!!!!!!}          |||")
+    print("  |||     ----------------------------------------------------------------------------          |||")
+    print("  |||                                                                                           |||")
+    print("  |||                                                                                           |||")
+    print("  |||                                                                                           |||")
+    print("  |||                                                             ____________                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  |||                                                             |       @  |                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  |||                                                             |          |                  |||")
+    print("  #################################################################################################")
+    print("  #################################################################################################")
+    print("  #################################################################################################")
+    print("\n")
+    input("                                     press enter to open the door......                            ")
+    input("                    AND...............................it is locked.  Press enter again please.     ")
+    print("             THERE IS NOTHING IN HERE , DUDE ! WHY DID YOU BELIEVE IN RICHARD'S LIE!!!!!!!!!!!!!!!!")
+    input("=============================================THANKS FOR PLAYING!=============================================")
+    print("                                                                                 (and being an ideat.hahaha!)")
+else:
+    print("=============================================THANKS FOR PLAYING!=============================================")
+
